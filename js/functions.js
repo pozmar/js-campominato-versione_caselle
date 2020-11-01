d9cdb234732c12c34a2c20b441d29502dfdf8e89
Martina <martina.pozzerle@libero.it>
2020-11-01 15:53:48 +0100

    #finito

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,20 @@
 //funzione che mi dice se i numeri si trovano dentro a un array
 function isInArray(num, arrNum){
   for(var i = 0; i < arrNum.length; i++ ){
-    if (num == arrNum[i]){
+    if (num === arrNum[i]){
       return true;
     }
   }
   return false;
+}
+//creo funzione per generare i numeri: parametri: numeri random, numero massimo e minimo. Crea un contenitore per le mine; finchè il numero dei numeri random è minore alle mine e se il numero è diverso da quello dell'array, inseriscilo dentro a quest'ultimo. dalla funzione mi ritorna la var mine
+function generator(randomNumbers, min, max){
+  var mine = [];
+  while(mine.length < randomNumbers){
+    var actualNumbers = Math.floor(Math.random() * ((max + 1) - min) + min);
+    if(!isInArray(actualNumbers, mine)){
+      mine.push(actualNumbers);
+    }
+  }
+  return mine;
 }
--- js/main.js
+++ js/main.js
@@ -1,39 +1,103 @@
 //costanti
 
 var MAX_NUM_EASY = 100;
-var MAX_NUM_MEDIUM = 80;
-var MAX_NUM_HARD = 50;
+var MAX_NUM_MEDIUM = 64;
+var MAX_NUM_HARD = 36;
 var MIN_NUM = 1;
 var NUM_MINE = 16;
-var ROUND = 84;
-var mine = [];
-//creo i 16 numeri random
-while (mine.length < NUM_MINE){
-  var randomNumbers = Math.floor(Math.random() * ((MAX_NUM_EASY + 1) - MIN_NUM) + MIN_NUM);
-  if(!isInArray(randomNumbers, mine)){
-    mine.push(randomNumbers);
+var mine = generator(NUM_MINE, MIN_NUM, MAX_NUM_EASY);
+//devo prendere gli slot dentro a ciascuna dashboard per lavorare sulle differenze dei tre livelli
+//con il querySelectorAll prendo tutti gli elementi dentro alla classe inserita
+var easySlots = document.querySelectorAll(".easy > * ")
+var mediumSlots = document.querySelectorAll(".medium > * ")
+var hardSlots = document.querySelectorAll(".hard > * ")
+//scegliere il livello
+//creo un contenitore per le tabelle
+var dashEl = document. getElementsByClassName("dashboard");
+//con il click seleziono il livello
+document.getElementById("level").addEventListener("change", function(event){
+  //prendo il valore del select
+  var difficulty = event.target.value;
+  //creo un switch che dica in base al livello quante mine e quante caselle ci sono
+  switch (difficulty) {
+    case "easy":
+    var mine = generator(NUM_MINE, MIN_NUM, MAX_NUM_EASY);
+      console.log(mine)
+    break;
+    case "medium":
+    var mine = generator(NUM_MINE, MIN_NUM, MAX_NUM_MEDIUM);
+    console.log(mine)
+    break;
+    case "hard":
+    var mine = generator(NUM_MINE, MIN_NUM, MAX_NUM_HARD);
+    console.log(mine)
+    break;
   }
-}
+  //con il ciclo for attraverso i tre div dashboard, in base al livello seleziono il display a ciascuna tabella
+  for(var i = 0; i < dashEl.length; i++){
+    if(dashEl[i].className.includes(difficulty)){
+      dashEl[i].style.display = "flex";
+    }else{
+      dashEl[i].style.display = "none";
+    }
+  }
+})
+//creo un ciclo per assegnare un valore a tutte le caselle. Versione Easy
 
-//creo un contenitore per le caselle
-var slotElements = document.getElementsByClassName("slot");
-//creo un ciclo per assegnare un valore a tutte le caselle
-for(var i = 0; i < slotElements.length; i++){
-  //assegno un valore alle caselle
-slotElements[i].value = i;
-//al click le caselle prendono un valore
-slotElements[i].addEventListener("click", function(event){
-  //prendo il valore numerico delle caselle quando clicco
-  var slotValue = parseInt(event.target.value);
-  //se il valore delle caselle si trova dentro all'array delle mine allora il bg sarà rosso
-  if(isInArray(slotValue, mine)){
-    event.target.style.backgroundColor = "red";
-  }else{
-    event.target.style.backgroundColor = "green";
-  }
-})
-slotElements[i].addEventListener("contextmenu", function(event){
-event.preventDefault();
-event.target.style.backgroundColor = "yellow";
-})
+for(var i = 0; i < easySlots.length; i++){
+  easySlots[i].value = i + 1;
+
+  easySlots[i].addEventListener("click", function(event){
+    var slotValue = parseInt(event.target.value);
+
+    if(isInArray(slotValue, mine)){
+      event.target.style.backgroundColor = "red";
+
+    }else{
+      event.target.style.backgroundColor = "green";
+    }
+  })
+  easySlots[i].addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    event.target.style.backgroundColor = "yellow";
+  })
+}
+//livello Medium
+
+for(var i = 0; i < mediumSlots.length; i++){
+  mediumSlots[i].value = i + 1;
+
+  mediumSlots[i].addEventListener("click", function(event){
+    var slotValue = parseInt(event.target.value);
+
+    if(isInArray(slotValue, mine)){
+      event.target.style.backgroundColor = "red";
+
+    }else{
+      event.target.style.backgroundColor = "green";
+    }
+  })
+  mediumSlots[i].addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    event.target.style.backgroundColor = "yellow";
+  })
+}
+//livello hard
+
+for(var i = 0; i < hardSlots.length; i++){
+  hardSlots[i].value = i + 1;
+  hardSlots[i].addEventListener("click", function(event){
+    var slotValue = parseInt(event.target.value);
+
+    if(isInArray(slotValue, mine)){
+      event.target.style.backgroundColor = "red";
+
+    }else{
+      event.target.style.backgroundColor = "green";
+    }
+  })
+  hardSlots[i].addEventListener("contextmenu", function(event){
+    event.preventDefault();
+    event.target.style.backgroundColor = "yellow";
+  })
 }
